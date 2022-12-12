88d3e8f8e087a71186d5bfa779001378352251e0
horibeni <horibeni31@gmail.com>
2022-12-12 20:56:03 +0100

    drawing note legs

--- widgets/staff.cpp
+++ widgets/staff.cpp
@@ -14,6 +14,7 @@
 #include <qwidget.h>
 #include <qwindowdefs.h>
 #include <vector>
+
 Staff::Staff(QWidget *parent)
     : QWidget(parent), treble(QImage("treble.png")), bass(QImage("bass.png")) {
   setAttribute(Qt::WA_StaticContents);
@@ -23,14 +24,10 @@ Staff::Staff(QWidget *parent)
 
 void Staff::paintEvent(QPaintEvent *event) {
 
-  int startx = 10;
-  int starty = 10;
-
-  int width = 300;
-  int gap = 10;
   QPainter painter(this);
   painter.drawImage(QRect(startx, starty - gap / 2, gap * 2, gap * 6), treble);
   painter.drawImage(QRect(startx, starty + gap * 6, gap * 2, gap * 3.5), bass);
+
   for (int i = 0; i < 11; i++) {
     if (i == 5)
       continue;
@@ -38,45 +35,52 @@ void Staff::paintEvent(QPaintEvent *event) {
                      starty + gap * i);
   }
 
-  int middleCpos = starty + 4 * gap + gap * 0.5;
-
   painter.setPen(Qt::NoPen);
   painter.setBrush(QBrush(Qt::black, Qt::SolidPattern));
   // draw fix notes
   for (int i = 0; i < fixNotes.size(); i++) {
-    int notePos = middleCpos - (fixNotes[i].pitch * gap * 0.5 +
-                                (fixNotes[i].octave - 4) * 7 * gap * 0.5);
-    painter.drawEllipse(startx + gap * 4, notePos, gap, gap);
-    if (fixNotes[i].accidental == Accidental::SHARP) {
-      painter.drawText(startx + gap * 4 - gap, notePos + gap, "#");
-    }
-    if (fixNotes[i].accidental == Accidental::FLAT) {
-      painter.drawText(startx + gap * 4 - gap, notePos + gap, "b");
-    }
-  }
+    drawNote(fixNotes[i], painter);
 
-  // draw pressed notes
-  for (int i = 0; i < pressedNotes.size(); i++) {
-    if (std::find(fixNotes.begin(), fixNotes.end(), pressedNotes[i]) !=
-        fixNotes.end()) {
-      painter.setBrush(QBrush(Qt::green, Qt::SolidPattern));
-
-    } else {
-      painter.setBrush(QBrush(Qt::red, Qt::SolidPattern));
-    }
-
-    int notePos = middleCpos - (pressedNotes[i].pitch * gap * 0.5 +
-                                (pressedNotes[i].octave - 4) * 7 * gap * 0.5);
-    painter.drawEllipse(startx + gap * 4, notePos, gap, gap);
-    if (pressedNotes[i].accidental == Accidental::SHARP) {
-      painter.drawText(startx + gap * 4 - gap, notePos + gap, "#");
-    }
-    if (pressedNotes[i].accidental == Accidental::FLAT) {
-      painter.drawText(startx + gap * 4 - gap, notePos + gap, "b");
+    // draw pressed notes
+    for (int i = 0; i < pressedNotes.size(); i++) {
+      if (std::find(fixNotes.begin(), fixNotes.end(), pressedNotes[i]) !=
+          fixNotes.end()) {
+        painter.setBrush(QBrush(Qt::green, Qt::SolidPattern));
+      } else {
+        painter.setBrush(QBrush(Qt::red, Qt::SolidPattern));
+      }
+      drawNote(pressedNotes[i], painter);
     }
   }
 }
-void Staff::resizeEvent(QResizeEvent *event) { update(); }
+
+void Staff::drawNote(Note n, QPainter &painter) {
+  int notePos =
+      middleCpos - (n.pitch * gap * 0.5 + (n.octave - 4) * 7 * gap * 0.5);
+  painter.setPen(QPen(Qt::black));
+  painter.drawLine(startx+gap*4 + gap, notePos + gap, startx+gap*4  + gap,
+                   notePos + gap - 3 * gap);
+
+  painter.setPen(Qt::NoPen);
+  painter.setBrush(QBrush(Qt::black, Qt::SolidPattern));
+  painter.drawEllipse(startx + gap * 4, notePos, gap, gap);
+  if (n.accidental == Accidental::SHARP) {
+    painter.drawText(startx + gap * 4 - gap, notePos + gap, "#");
+  }
+  if (n.accidental == Accidental::FLAT) {
+    painter.drawText(startx + gap * 4 - gap, notePos + gap, "b");
+  }
+}
+void Staff::resizeEvent(QResizeEvent *event) {
+  startx = 10;
+  starty = 10;
+  gap = 10;
+  middleCpos = starty + 4 * gap + gap * 0.5;
+  width = 300;
+  // todo responsive
+
+  update();
+}
 
 void Staff::AddNote(Note n, bool fixed) {
   if (!fixed) {
--- widgets/staff.h
+++ widgets/staff.h
@@ -4,26 +4,33 @@
 #include "midi/note.h"
 #include <qimage.h>
 #include <qobjectdefs.h>
+#include <qpainter.h>
 #include <qwidget.h>
 #include <vector>
 class Staff : public QWidget {
   Q_OBJECT
 public:
   Staff(QWidget *parent = nullptr);
-  
-  void AddNote(Note n , bool fixed = false);
-  void RemoveNote(Note n ,bool fixed = false);
+
+  void AddNote(Note n, bool fixed = false);
+  void RemoveNote(Note n, bool fixed = false);
+
 protected:
   void paintEvent(QPaintEvent *event) override;
   void resizeEvent(QResizeEvent *event) override;
+  void drawNote(Note n, QPainter &painter);
 
 private:
-QImage treble;
-QImage bass;
+  int startx;
+  int starty;
+  int gap;
+  int middleCpos;
+  int width;
+  QImage treble;
+  QImage bass;
   std::vector<Note> pressedNotes;
   std::vector<Note> fixNotes;
 
   QImage image;
-
 };
 #endif
